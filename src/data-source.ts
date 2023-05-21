c125fc3152d53eb5e02d50fc07deec34718d1f3e
Lucas <lucasspindolafernandes196@hotmail.com>
2023-05-21 16:01:06 -0300

    feat: createEntites-add-migration

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,6 +6,10 @@ import { DataSource, DataSourceOptions } from "typeorm";
 import { Restaurant } from "./entities/restaurant.entity";
 import { createTables1684524058296 } from "./migrations/1684524058296-createTables";
 import { createTables1684524123001 } from "./migrations/1684524123001-createTables";
+import { createTable21684695510954 } from "./migrations/1684695510954-createTable2";
+import { TypeRestaurant } from "./entities/typeRestaurant.entity";
+import { DayWeek } from "./entities/daysWeek.entity";
+import { OpeningHour } from "./entities/openingHours.entity";
 
 const setDataSourceConfig = (): DataSourceOptions => {
   const entitiesPath: string = path.join(__dirname, "./entities/**.{js,ts}");
@@ -43,8 +47,12 @@ const setDataSourceConfig = (): DataSourceOptions => {
     database: process.env.DB,
     synchronize: false,
     logging: true,
-    entities: [Restaurant],
-    migrations: [createTables1684524058296, createTables1684524123001],
+    entities: [Restaurant, TypeRestaurant, DayWeek, OpeningHour],
+    migrations: [
+      createTables1684524058296,
+      createTables1684524123001,
+      createTable21684695510954,
+    ],
   };
 };
 
